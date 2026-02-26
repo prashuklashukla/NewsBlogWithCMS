836f572731503abf35d238519f6f3ceeeebf300e
prashant shukla <ankurshukla051@gamil.com>
2026-02-26 20:00:55 +0530

    third commit

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const expressLayout = require("express-ejs-layouts");
+const session = require("express-session");
+const flash = require("connect-flash");
+
 const app = express();
 require("dotenv").config();
 
+//Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(expressLayout);
+app.set('layout', 'layout');
+
+
+// views engine
+app.set("view engine", "ejs");
+
+//database connection
+mongoose.connect(process.env.MONGODB_URI)
 
 
 app.get("/", (req, res) => {
